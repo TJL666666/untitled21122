ca428e4edf2cf2e50f65d0d003a648fc6fea96df
TJL666666 <1846897466@qq.com>
2021-12-02 08:21:21 +0800

    third commit hot fix second

--- src/main/java/com/he/Hello.java
+++ src/main/java/com/he/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println(22222);
         System.out.println(33333);
         System.out.println(444444);
+        System.out.println("hot-// FIXME: 2021/12/2 hot-fix" );
     }
 }
